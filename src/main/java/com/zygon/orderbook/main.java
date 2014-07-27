2c29bf3dcadad5bcdbd6ed4bbcf0ec9fef9db1b1
zygon4 <dcharubini@gmail.com>
2014-07-27 18:35:31 -0400

    updates - mostly complete

--- src/main/java/com/zygon/orderbook/main.java
+++ src/main/java/com/zygon/orderbook/main.java
@@ -1,5 +1,10 @@
 package com.zygon.orderbook;
 
+import com.xeiam.xchange.currency.CurrencyPair;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Random;
+
 public class main {
     /*
      * So.. order book
@@ -10,4 +15,40 @@ public class main {
      * * 
      * 
      */
+    
+    ///// helper functions for testing /////
+    
+    private static int ID = 0;
+    private static Order create (com.xeiam.xchange.dto.trade.LimitOrder.OrderType type, BigDecimal ammount, BigDecimal price) {
+        ID++;
+        return new Order(new com.xeiam.xchange.dto.trade.LimitOrder(type, ammount, CurrencyPair.BTC_USD, String.valueOf(ID), new Date(), price));
+    }
+    
+    public static void main(String[] args) {
+        OrderBook book = new OrderBook();
+        
+        for (int i = 10; i < 20; i++) {
+            for (int j = 1; j < 1 + new Random().nextInt(5); j++) {
+                book.addOrder(create(com.xeiam.xchange.dto.trade.LimitOrder.OrderType.ASK, BigDecimal.valueOf(new Random().nextInt(10)), BigDecimal.valueOf(i)));
+            }
+//            System.out.println(book);
+        }
+        
+        
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 1 + new Random().nextInt(5); j++) {
+                book.addOrder(create(com.xeiam.xchange.dto.trade.LimitOrder.OrderType.BID, BigDecimal.valueOf(new Random().nextInt(10)), BigDecimal.valueOf(i)));
+            }
+//            System.out.println(book);
+        }
+        
+        System.out.println(book);
+        
+        // Cross the price barrier
+        
+        // allowing zero fill for testing
+        book.addOrder(create(com.xeiam.xchange.dto.trade.LimitOrder.OrderType.BID, BigDecimal.valueOf(15 + new Random().nextInt(25)), BigDecimal.valueOf(10 + new Random().nextInt(8))));
+        
+        System.out.println(book);
+    }
 }
